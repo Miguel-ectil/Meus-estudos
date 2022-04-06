d3bb58255005b2e987f05633b82d2ab0405cbbb2
Miguel ectil <87404714+Miguel-ectil@users.noreply.github.com>
2022-04-06 08:08:00 -0300

    [ADD] TypeScript

---
+++ Estudo_TypeScript/Meus_testes/modulo_1/teste_1.js
@@ -0,0 +1,10 @@
+"use strict";
+console.log('==== Desafio 1! Transformando códigos Python em TypeScript ====');
+let nombre = 'Miguel Ectil';
+console.log('Olá', nombre, 'prazer em te conhecer!');
+// ==> Data de nascimento
+console.log('==== Desafio 2! ====');
+let dia = 7;
+let mes = 'março';
+let ano = 2005;
+console.log('Você nasceu no dia', dia, 'de', mes, 'de', ano);
---
+++ Estudo_TypeScript/Meus_testes/modulo_1/teste_1.ts
@@ -0,0 +1,16 @@
+
+console.log('==== Desafio 1! Transformando códigos Python em TypeScript ====');
+
+let nombre: string = 'Miguel Ectil';
+
+console.log('Olá', nombre, 'prazer em te conhecer!');
+
+// ==> Data de nascimento
+
+console.log('==== Desafio 2! ====');
+
+let dia: number = 7 ;
+let mes: string = 'março';
+let ano: number = 2005;
+
+console.log('Você nasceu no dia', dia, 'de', mes, 'de', ano)
