7f19b4fb7f554b3500f074bb14f275a78d88bd15
Miguel ectil <87404714+Miguel-ectil@users.noreply.github.com>
2022-04-20 16:24:39 -0300

    [ADD] Adicionando

---
+++ Estudo_Vue/Com_Matheus/2_projeto_vue/src/components/MultiplosEventos.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <button @click="primeiro($event), segundo($event), terceiro(txt3,  $event)">Ativar multiplos eventos</button>
+        <p>{{ multiplos_eventos }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'MultiplosEventos',
+        data() {
+            return {
+                multiplos_eventos: "",
+                txt3: "Terceiro evento"
+            }
+        },
+        methods: {
+            primeiro(e) {
+                this.multiplos_eventos = "Primeiro evento ativado!"
+            },
+            segundo(e) {
+                setTimeout(() => this.multiplos_eventos = "Segundo evento!",500 )
+            },
+            terceiro(txt, e) {
+                setTimeout(() => this.multiplos_eventos = txt, 1500)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        margin: 60px;
+    }
+</style>
