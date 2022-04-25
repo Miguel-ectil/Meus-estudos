11ddb346614536e0fe9bc8d5ee1c6cea35d46c05
Miguel ectil <87404714+Miguel-ectil@users.noreply.github.com>
2022-04-25 17:13:01 -0300

    [UPD] Atualizandop estudos de vue

---
+++ Estudo_Vue/Com_Matheus/3_make_your_burguer/src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer id="footer">
+    <p>Make Your Burger &copy; 2022</p>
+  </footer>  
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+}
+</script>
+
+<style>
+  #footer {
+    height: 320px;
+    background-color: #222;
+    border-top: 4px solid #111;
+    color: #FCBA03;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+  }
+</style>
---
+++ Estudo_Vue/Com_Matheus/3_make_your_burguer/src/views/Pedidos.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="main-container">
+    <h1>PEDIDOS</h1>
+  </div>
+</template>
