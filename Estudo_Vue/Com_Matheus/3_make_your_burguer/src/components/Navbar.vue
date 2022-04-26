f876922560ecb842020cea17403d9b61862ac662
Miguel ectil <87404714+Miguel-ectil@users.noreply.github.com>
2022-04-26 15:12:58 -0300

    [UPD] Atualização

---
+++ Estudo_Vue/Com_Matheus/3_make_your_burguer/src/components/Navbar.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="nav">
+    <router-link to="/" id="logo-url">
+        <img :src="logo" :alt="alt" id="logo">
+    </router-link>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/pedidos">Pedidos</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Navbar',
+    props: ["logo", "alt"]
+}
+</script>
+<style scoped>
+  #nav {
+    background-color: #222;
+    border-bottom: 4px solid #111;
+    padding: 15px 50px;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+  }
+
+    #nav #logo-url {
+        margin: auto;
+        margin-left: 0;
+    }
+
+    #logo {
+        width: 40px;
+        height: 40px;
+    }
+
+    #nav a {
+        color: #FCBA03;
+        text-decoration: none;
+        margin: 12px;
+        transition: .5s;
+    }
+
+    #nav a:hover {
+        color: #FFF;
+    }
+
+</style>
