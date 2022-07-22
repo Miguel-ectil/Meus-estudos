bfbc164c922aed74ef7f535cbdc7a0eca29b1a06
Miguel-ectil <ectilmiguelmiguelectil@gmail.com>
2022-07-22 13:34:53 -0300

    [UPD] Adicionando component estilizando e fazendo rotas com link

---
+++ Estudo_next/com_matheus-batti/hello_world/components/Footer.js
@@ -0,0 +1,9 @@
+import styles from '../styles/Footer.module.css'
+
+export default function Fonter() {
+  return (
+    <footer className={styles.footer}>
+      <p>Curso de Next.js &copy; 2022</p>
+    </footer>
+  )  
+}
