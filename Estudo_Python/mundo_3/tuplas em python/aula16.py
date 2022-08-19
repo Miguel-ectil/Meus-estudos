96c53a33b141fa6a3e54a7a2a7f101cfc98841c6
miguel-ectil <miguelectilectilmiguel@gmail.com>
2022-08-19 15:39:52 -0300

    [ADD] Aula 16 explicando e dando exemplos de manipulação de tuplas

---
+++ Estudo_Python/mundo_3/tuplas em python/aula16.py
@@ -0,0 +1,51 @@
+
+# print('-='*24)
+# print('-=-=-=-=- Aprendendo a manipular tuplas -=-=-=-=')
+# print('-='*24)
+
+# lanche = ('Hambúrguer', 'Suco', 'Pizza', 'Pudin', 'Batata Frita')
+# print(lanche) # [0], [-2], [2:], [:2], [-2:], [1:3], [-3:]
+# # também podemos imprimi-lo assim
+# print(sorted(lanche))
+
+# print('=======================================================' )
+
+# # Outra forma de manipular os dados da variavel tupla
+
+# print('------ maneira mais simples usando for ------')
+# for comida in lanche:
+#     print(f'Eu vou comer {comida} ')
+# print('---------------------------------------------')
+
+# print('------ outra maneira, pegando a posição ------')
+# for cont in range(0, len(lanche)):
+#     print(f'Eu vou comer {lanche[cont]} que esta na posição {cont}')
+    
+# print('----! Mesmo exemplo sem range !----' )
+
+# # Mais uma forma de manipular os valores da variavel tupla 
+# for pos, comida in enumerate(lanche):
+#     print(f'Eu vou comer {comida} que esta na posição {pos}')
+# print('Comi pra caramba!')
+# print('-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-' )
+
+
+# # Tuplas são imutáveis
+# print('provando que tuplas são imutáveis')
+# # lanche[1] = 'Refrigerante' 
+# # print(lanche[1])
+
+print('===================================================================')
+
+a = (2, 5, 4)
+b = (5, 8, 1, 2)
+c = b + a
+print(c)
+print(c.count(4))
+print(c.index(5, 1)) # (c.index(5, 1))
+
+print('===================================================================')
+
+pessoa = ('Miguel', 17, 'M', 1.69)
+# del(pessoa)
+print(pessoa)
