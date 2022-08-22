ff77d187da39399809439b305a57bfc000b0cf12
miguel-ectil <miguelectilectilmiguel@gmail.com>
2022-08-22 17:31:53 -0300

    [ADD] fazendo o Exercicio 73, ainda não 100% terminado

---
+++ Estudo_Python/mundo_3/tuplas em python/exercicio 73.py
@@ -0,0 +1,18 @@
+  
+brasileirao = ('Palmeiras', 'Fluminense', 'Flamengo', 'Corinthians', 'Athletico-PR', 
+'Internacional', 'Atlético-MG','Santos', 'América-MG', 'Bragantino', 'Goiás', 'São Paulo',
+'Fortaleza', 'Botafogo', 'Ceará', 'Cuiabá', 'Avaí', 'Coritiba', 'Atlético-GO', 'Juventude')
+
+print("A lista de times do Brasileirão:", brasileirao)
+print('-=' * 16)
+
+print('Os 5 primeiros são', brasileirao[0:5])
+print('-=' * 16)
+
+print('Os 4 ùltimos são', brasileirao[16:21])
+print('-=' * 16)
+
+print('Os times em ordem alfabética:')
+print('-=' * 16)
+
+print('O', brasileirao[7], 'esta na 8ª posição')
