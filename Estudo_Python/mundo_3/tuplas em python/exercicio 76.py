045abe97c4a4a0e43f67d317ad9e8d9e093e0b0f
miguel-ectil <miguelectilectilmiguel@gmail.com>
2022-09-09 11:54:59 -0300

    [UPD] Melhorando os códigos python

--- Estudo_Python/mundo_3/tuplas em python/exercicio 76.py
+++ Estudo_Python/mundo_3/tuplas em python/exercicio 76.py
@@ -8,7 +8,7 @@ lista = ("Lápis", 1.75, "Borracha", 2.00, "Caderno", 15.90, "Estojo",  25.00,
 
 for pos in range(0, len(lista)):
   if pos % 2 == 0:
-    print(f'{lista[pos]:.<30}', end='')
+    print(f'{lista[pos]:.<20}', end='')
   else:
     print(f'R${lista[pos]:>7.2f}')
 print('-' * 40)
