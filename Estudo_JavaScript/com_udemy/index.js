e1a4519b2a4032075a6b9659b28a6b05bf2af138
Miguel-ectil <ectilmiguelmiguelectil@gmail.com>
2023-10-15 19:10:13 -0300

    [ADD and UPD] Estudando JavaScript

--- Estudo_JavaScript/com_udemy/index.js
+++ Estudo_JavaScript/com_udemy/index.js
@@ -8,9 +8,11 @@ let anoNascimento;
  
 indiceMassaCorporal = peso / (alturaEmM * alturaEmM);
 anoNascimento = 2019 - idade;
- 
+
+//  arredondado o resultado para o número mais proximo que tiver
+let indiceMassaCorporal1 = Math.round(indiceMassaCorporal)
+
 // template strings
- 
 console.log(`${nome} ${sobrenome} tem ${idade} anos, pesa ${peso} kg`);
-console.log(`tem ${alturaEmM} de altura e seu IMC é de ${indiceMassaCorporal}`);
+console.log(`tem ${alturaEmM} de altura e seu IMC é de ${indiceMassaCorporal1}`);
 console.log(`${nome} nasceu em ${anoNascimento}.`);
