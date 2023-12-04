bfac8ae50f638cdee3641fdea47aae02f7e1f649
Miguel-ectil <ectilmiguelmiguelectil@gmail.com>
2023-12-04 09:44:44 -0300

    [ADD]  EStudadndo golang

---
+++ Estudo_Golang/estudo_fullcycle/2/main.go
@@ -0,0 +1,21 @@
+package main
+
+const a = "Hello, World!"
+
+var (
+	b bool    = true
+	c int     = 10
+	d string  = "Miguel"
+	e float64 = 1.2
+)
+
+func main() {
+	a := "X" //string
+	a = "fd"
+
+	println(a)
+}
+
+func X() {
+
+}
