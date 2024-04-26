3e7e012a97c8ce58b8ddffbf0ee0a53cb1f32402
Miguel-ectil <ectilmiguelmiguelectil@gmail.com>
2024-04-26 08:59:04 -0300

    [ADD] Adicionando arquivo python que verifica os anos bissextos dentreo de um intervalo de anos exercicio da pre2 do beecrow

---
+++ Estudo_Python/faculdade/codigos_linguagem/pre2/anos_bissextos.py
@@ -0,0 +1,12 @@
+
+inicio = int(input())
+fim = int(input())
+cont = 0
+ 
+while inicio <= fim:
+  if inicio % 4 == 0 and inicio % 100 != 0 or inicio % 400 == 0:
+    cont += 1 
+    print(f'{inicio}')
+  inicio += 1
+  
+print(f'bissextos: {cont}')
