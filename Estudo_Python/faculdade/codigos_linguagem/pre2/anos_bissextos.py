0ff24c59199f5482b1804171816577c92c999101
Miguel-ectil <ectilmiguelmiguelectil@gmail.com>
2024-04-30 13:32:48 -0300

    [ADD and UPD] Adicionando mais conteudo aos códigos 'Tabuada e Anos Bissextos' assim melhorando elas

--- Estudo_Python/faculdade/codigos_linguagem/pre2/anos_bissextos.py
+++ Estudo_Python/faculdade/codigos_linguagem/pre2/anos_bissextos.py
@@ -2,11 +2,12 @@
 inicio = int(input())
 fim = int(input())
 cont = 0
- 
-while inicio <= fim:
-  if inicio % 4 == 0 and inicio % 100 != 0 or inicio % 400 == 0:
-    cont += 1 
-    print(f'{inicio}')
-  inicio += 1
-  
-print(f'bissextos: {cont}')
+
+if 0 <= inicio <= fim <= 9999:
+  while inicio <= fim:
+    if inicio % 4 == 0 and inicio % 100 != 0 or inicio % 400 == 0:
+      cont += 1 
+      print(f'{inicio}')
+    inicio += 1
+    
+  print(f'bissextos: {cont}')
--- Estudo_Python/faculdade/codigos_linguagem/pre2/tabuada.py
+++ Estudo_Python/faculdade/codigos_linguagem/pre2/tabuada.py
@@ -1,9 +1,10 @@
 
-num = int(input())
+N = int(input())
 
-for c in range(1, 11):
-  num_multi = num * c
-  print(f'{num} x {c} = {num_multi}')
+if 0 <= N <= 10:
+  for c in range(1, 11):
+    num_multi = N * c
+    print(f'{N} x {c} = {num_multi}')
   
   # outra forma de printar na tela
 #   print('{} x {:2} = {}'.format(num, c, num*c))
