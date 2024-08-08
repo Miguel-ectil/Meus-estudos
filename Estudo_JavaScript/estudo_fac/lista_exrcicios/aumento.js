438946e2f361d7e197da49e159f04fc31445b3a7
Miguel-ectil <ectilmiguelmiguelectil@gmail.com>
2024-08-08 09:37:41 -0300

    [ADD] Adicionando dois arquivos que são os exercícios passados pelo prof

---
+++ Estudo_JavaScript/estudo_fac/lista_exrcicios/aumento.js
@@ -0,0 +1,7 @@
+nome = 'Miguel'
+salario = 2000
+
+aumento = salario * 10 / 100
+salarioFinal = aumento + salario
+
+console.log(salarioFinal)
---
+++ Estudo_JavaScript/estudo_fac/lista_exrcicios/media.js
@@ -0,0 +1,7 @@
+let n1 = 2
+let n2 = 2
+let n3 = 4
+
+const media = (n1 + n2 + n3) / 3
+
+console.log(media)
